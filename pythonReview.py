e8f425bb5b55d9b8a59ec07b1006dcf1ecd158c7
mayayl23-meet <109899357+mayayl23-meet@users.noreply.github.com>
2022-07-25 19:20:41 +0300

    Add files via upload

---
+++ pythonReview.py
@@ -0,0 +1,35 @@
+def create_youtube_video(title,description):
+	videodict = {"title":title, "description":description, "likes":0, "dislikes":0, "comments": {"username":{}}}
+	return videodict
+
+	
+
+def like(youtubevideo):
+	if "likes" in youtubevideo:
+		youtubevideo["likes"] += 1
+
+		return youtubevideo
+
+
+def dislike(youtubevideo):
+	if "dislikes" in youtubevideo:
+		youtubevideo["dislikes"] += 1
+
+		return youtubevideo
+
+
+def add_comment(youtubevideo, username, comment_text):
+	youtubevideo = videodict
+	comments[username] = comment_text
+
+	return youtubevideo
+
+dict1 = create_youtube_video("a","b")
+like(dict1)
+dislike(dict1)
+add_comment(dict1, "maya", "hello")
+
+
+
+
+# newvideodict = {"title": How To Make Coffee, "description": a short video about how to make the best coffee, "likes": 495, "dislikes": 0}
